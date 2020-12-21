0607f15902350fd4b6ed764012708011c2f61a51
UserBlackBox <userblackbox@tutanota.com>
2020-12-21 10:45:23 -0500

    rss feed

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 from flask import Flask, request, render_template, redirect, url_for, abort
 import os
-from blog_render import to_html
+from blog_render import to_html, to_html_noclass
 import json
+from feedgen.feed import FeedGenerator
+import datetime
+import pytz
+
+url = 'http://127.0.0.1:5000'
+owner = 'YourNameHere'
 
 app = Flask(__name__)
 
@@ -32,6 +38,7 @@ def blog_home():
         data = json.load(open('content/'+articles[f], 'r'))
         articles[f] = (data['title'], data['date'], data['slug'])
     articles.sort(key=lambda f:f[1])
+    articles = articles[::-1]
     return render_template('blog.html', articles=articles)
 
 @app.route('/blog/<slug>/')
@@ -41,6 +48,35 @@ def article(slug):
     html = to_html('content/'+slug+'.mkd')
     data = json.load(open('content/'+slug+'.json', 'r'))
     return render_template('article.html', title=data['title'], date=data['date'], content=html)
+
+@app.route('/rss.xml')
+def rss():
+    articles = []
+    for f in os.listdir('content'):
+        if f.endswith('.json'):
+            articles.append(f)
+    for f in range(len(articles)):
+        data = json.load(open('content/'+articles[f], 'r'))
+        articles[f] = (data['title'], data['date'], data['slug'])
+    feed = FeedGenerator()
+    feed.title("YourNameHere's Blog")
+    feed.author({'name':owner,'email':'user@domain.com'})
+    feed.link(href=url, rel='self')
+    feed.language('en-CA')
+    feed.updated(pytz.timezone("America/Toronto").localize(datetime.datetime.now()))
+    feed.subtitle(subtitle="Some description of the blog")
+    for f in articles:
+        entry = feed.add_entry()
+        entry.id(url+'/blog/'+f[2])
+        entry.title(f[0])
+        entry.link(href=url+'/blog/'+f[2])
+        entry.description(description=to_html_noclass('content/'+f[2]+'.mkd'))
+        entry.author({'name':owner,'email':'user@domain.com'})
+        date = pytz.timezone("America/Toronto").localize(datetime.datetime.strptime(f[1],'%Y/%m/%d'))
+        entry.pubDate(date)
+    return feed.rss_str(pretty=True)
+
+
 
 @app.errorhandler(404)
 def not_found(e):
