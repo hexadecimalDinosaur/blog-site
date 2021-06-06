6d9739c40f17757de3a67c8efc1555e0836a3284
UserBlackBox <userblackbox@tutanota.com>
2021-06-06 12:28:53 -0400

    create markdown renderer for table of contents

--- blog_render.py
+++ blog_render.py
@@ -12,6 +12,20 @@ def to_html(file):
         }
     })
 
+@lru_cache(maxsize=32, expires=60)
+def to_html_toc(file):
+    f = open(file,'r')
+    md = f.read()
+    convert = markdown.Markdown(extensions=['toc', 'codehilite', 'extra', 'smarty'], extension_configs={
+        'codehilite':{
+            'linenums':True,
+            'guess_lang':False
+        }
+    })
+    html = convert.convert(md)
+    toc = convert.toc
+    return html, toc
+
 def to_html_noclass(file):
     f = open(file,'r')
     md = f.read()
