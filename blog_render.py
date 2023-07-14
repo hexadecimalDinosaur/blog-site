9631ab3e9d3e3fe49e9ff4acc09f0fbb1a4f14fe
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2023-07-14 12:24:40 -0400

    add katex math rendering support

--- blog_render.py
+++ blog_render.py
@@ -2,22 +2,22 @@ import markdown
 from lru import lru_cache
 from bs4 import BeautifulSoup
 
-@lru_cache(maxsize=32, expires=60)
+@lru_cache(maxsize=32, expires=300)
 def to_html(file):
     f = open(file,'r')
     md = f.read()
-    return markdown.markdown(md, extensions=['codehilite', 'extra', 'smarty', 'mdx_truly_sane_lists', 'footnotes'], extension_configs={
+    return markdown.markdown(md, extensions=['codehilite', 'extra', 'smarty', 'mdx_truly_sane_lists', 'footnotes', 'markdown_katex'], extension_configs={
         'codehilite':{
             'linenums':True,
             'guess_lang':False
         }
     })
 
-@lru_cache(maxsize=32, expires=60)
+@lru_cache(maxsize=32, expires=300)
 def to_html_toc(file):
     f = open(file,'r')
     md = f.read()
-    convert = markdown.Markdown(extensions=['toc', 'codehilite', 'extra', 'smarty', 'mdx_truly_sane_lists', 'footnotes'], extension_configs={
+    convert = markdown.Markdown(extensions=['toc', 'codehilite', 'extra', 'smarty', 'mdx_truly_sane_lists', 'footnotes', 'markdown_katex'], extension_configs={
         'codehilite':{
             'linenums':True,
             'guess_lang':False
@@ -32,10 +32,11 @@ def to_html_toc(file):
     toc = convert.toc
     return html, toc
 
+@lru_cache(maxsize=32, expires=300)
 def to_html_noclass(file):
     f = open(file,'r')
     md = f.read()
-    return markdown.markdown(md, extensions=['codehilite', 'extra', 'smarty', 'mdx_truly_sane_lists', 'footnotes'], extension_configs={
+    return markdown.markdown(md, extensions=['codehilite', 'extra', 'smarty', 'mdx_truly_sane_lists', 'footnotes', 'markdown_katex'], extension_configs={
         'codehilite':{
             'linenums':True,
             'guess_lang':False,
